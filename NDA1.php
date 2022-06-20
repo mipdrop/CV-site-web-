ad1fa04fcf6e400754c02f698a395dd231f30902
mipdrop <92934805+mipdrop@users.noreply.github.com>
2022-06-20 12:22:03 +0200

    Add files via upload

---
+++ NDA1.php
@@ -0,0 +1,109 @@
+<head>
+<meta charset = "utf-8"/>
+</head> 
+<h1> Accord de confidentialit&eacute </h1>  
+
+<?php echo $_GET['name'] ?>, 
+
+<p>Entre,</p> 
+
+<?php echo $_GET['Prénom'] ?>, 
+<?php echo $_GET['Nom'] ?>, 
+ 
+
+n&eacute le <?php echo $_GET['Né'] ?>, habitant au
+
+<?php echo $_GET['Habitant'] ?>, 
+
+dont l adresse mail est
+
+<?php echo $_GET['Mail'] ?>, 
+
+au capital de, 
+<?php echo $_GET['Capital'] ?>, 
+
+dont le siege social est à
+
+<?php echo $_GET['Siège'] ?>, 
+
+
+ayant pour num&eacutero SIREN le 
+
+<?php echo $_GET['SIREN'] ?>, 
+
+
+<p>d’une part.</p>
+
+
+<p>Et,</p>
+
+<?php echo $_GET['NomY'] ?>,
+<?php echo $_GET['PrénomY'] ?>, 
+n&eacute le 
+<?php echo $_GET['Née'] ?>
+
+habitant au
+
+<?php echo $_GET['HabitantY'] ?>, 
+
+l adresse mail est
+
+<?php echo $_GET['MailY'] ?>, 
+
+au capital de, 
+<?php echo $_GET['CapitalY'] ?>, 
+
+dont le siege social est à
+
+<?php echo $_GET['SiègeY'] ?>, 
+
+ayant pour num&eacutero SIREN le 
+
+<?php echo $_GET['SIRENY'] ?>, 
+
+
+<p>d’autre part.</p>
+
+<p>Dans le cadre de l’&eacutetude d’une opportunité de partenariat, chacune des parties peut etre amenée à r&eacutev&eacuteler à l’autre partie des informations confidentielles la concernant à l occasion de leurs entrevues, r&eacuteunions de présentation, r&eacuteunions de travail, n&eacutegociations, discussions, pourparlers ou toute autre forme de rencontre.</p>
+
+<p>1. Informations confidentielles</p>
+
+Les termes « informations confidentielles » désignent tous les documents et informations, de quelque nature que ce soit, que l’un des parties a pu communiquer à l’autre partie, sous quelque forme que ce soit et notamment oralement au cours de leurs Relations professionnelles. Le projet confidentiel pour nom. 
+Ce projet compte : 
+
+
+<p>2. Obligations des parties </p>
+Chaque partie s engage, d’une part, à ne pas divulguer, ni à communiquer à quiconque tout ou partie des Informations confidentielles et, d’autre part, à prendre toute disposition pour que cette confidentialité soit préservée.
+Chaque partie, s engage à ne faire aucun usage des informations confidentielles dans un but autre que celui des Relations professionnelles.
+Par les présentes, chacune des Parties s’engage irrévocablement, et se porte fort de ce que ses Affiliés et Représentants s’engagent, sans condition, limitation ou restriction, à :
+
+n'utiliser les Informations Confidentielles que pour son propre compte et aux seules fins d’étudier les conditions de réalisation du Projet, et notamment s’abstenir d'utiliser les Informations Confidentielles d'une quelconque manière qui pourraient porter atteinte à l’autre Partie ou ses Affiliés, à leurs activités industrielles, commerciales et financières, ou de façon générale qui pourrait porter atteinte à leur valorisation;
+
+assurer la plus stricte confidentialité des Informations Confidentielles et à ne pas les reproduire, à ne pas les divulguer ou communiquer, en tout ou partie, de quelque manière que ce soit, à des personnes ou entités autres que celles visées au paragraphe c) ci-dessous. 
+Dans l’hypothese où l’une ou l’autre des Parties ou un de ses Représentants serait dans l’obligation légale ou r&eacuteglementaire, en vertu d'une décision de justice ou de toute loi ou réglementation applicable, de divulguer des Informations Confidentielles à des autorités administratives, arbitrales ou judiciaires, elle s’engage à fournir dans les plus brefs délais à l’autre Partie (dans la mesure où cela est légalement possible) la notification préalable et écrite d’une telle obligation afin qu’elle puisse, le cas échéant, prendre toute mesure ou action de protection, et qu’en toutes circonstances soit possible une consultation préalable avec l’autre Partie sur l’étendue et le calendrier de la divulgation envisagée ;
+
+limiter, par tous moyens appropriés, la diffusion totale ou partielle ou l’utilisation des Informations Confidentielles à ceux des Représentants dont l’intervention pour l’étude et les négociations relatives au Projet s’avérerait nécessaire, avec l’engagement d’informer préalablement ces personnes de la nature confidentielle des Informations Confidentielles, à leur imposer les mêmes obligations de confidentialité que celles souscrites aux termes du présent Accord de Confidentialité, et à prendre toute disposition nécessaire pour leur faire respecter le présent Accord de Confidentialité ;
+
+prendre toutes les mesures n&eacutecessaires, a minima comme elle le ferait pour ses propres données, pour que les Informations Confidentielles et toutes copies soient conservées en toute sécurité et soient protégées contre le vol, la divulgation ou tout autre forme d’accès non autorisé ;
+ 
+
+
+informer l’autre Partie de toute violation ou non-respect des obligations impos&eacutees par le pr&eacutesent Accord de Confidentialit&eacute dont elle aurait connaissance et fournir toute l'assistance possible pour minimiser les effets d'une telle violation ou d'un tel non-respect.
+
+Informer, au préalable, l’autre Partie de l’identit&eacute des Affiliés/Partenaires pouvant avoir accès à tout ou partie des donn&eacutees confidentielles objet du présent Accord, (sous réserve exclusive que les Parties se sont mises d’accord sur le principe de cet échange) avant de divulguer une quelconque information à cet Affili&eacutes/Partenaire tiers à la pr&eacutesente Convention. 
+
+
+<p>3. Propri&eacutet&eacute</p>
+Les informations confidentielles, de toute nature et quelle que soit leur forme, fournies par une partie demeurent sa propri&eacutet&eacute exclusive.
+Les parties s’engagent à se restituer mutuellement, sur simple demande tous les documents, de quelque nature que ce soit et quelle que soit leur forme et à ne pas en garder de copie ou reproduction.
+Chaque partie s’engage à ne pas revendiquer de droits de propriétés industrielles, litt&eacuteraires ou artistiques sur les Informations confidentielles et sur les connaissances établies à partir de ces Informations confidentielles.
+
+<p>4. Dur&eacutee</p>
+Le présent accord entre en vigueur à compter du jour de sa signature par chacune des parties.
+Les obligations d&eacutefinies par le présent article resteront en vigueur pour une durée de deux ans a compter de son entrée en vigueur.
+
+<p>5. Loi applicable & tribunal compétent</p>
+Le présent accord est régi par la loi française.
+Tout litige susceptible de survenir entre les parties relatif au présent accord sera de la compétence exclusive du tribunal de grande instance de Paris.
+
+Signé le ………..<?php echo $_GET['SIGNE'] ?>
